e045623c5ba4f1b16d602c76775247b4180b1cfa
Bernardo Raposo <74255317+RaposoMonteiro@users.noreply.github.com>
2024-06-23 13:19:11 -0300

    Vite config done

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Import our custom CSS.
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JS.
+import * as bootstrap from 'bootstrap';
+
+// Import individually Javascript plugins.
+// import Alert from 'bootstrap';
+
+// Import specify wich plugins I need.
+// import {Tooltip, Toast, Popover} from 'bootstrap';
