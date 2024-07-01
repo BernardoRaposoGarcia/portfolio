0593aaad3179d3ee4bf742bb695b2e85490ae7c0
Bernardo Raposo <74255317+RaposoMonteiro@users.noreply.github.com>
2024-07-01 00:34:48 -0300

    importmap error

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,9 @@ import * as bootstrap from 'bootstrap';
 
 // Import specify wich plugins I need.
 // import {Tooltip, Toast, Popover} from 'bootstrap';
+{
+    "imports": {
+        "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
+        "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.esm.min.js"
+      }
+}
