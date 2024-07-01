34a5bf57e0208e2335df82b25a4263cd7bd1d94d
Bernardo Raposo <74255317+RaposoMonteiro@users.noreply.github.com>
2024-07-01 00:59:57 -0300

    import error

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 // Import our custom CSS.
 import '../scss/styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 
 // Import all of Bootstrap's JS.
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 
 // Import individually Javascript plugins.
 // import Alert from 'bootstrap';
